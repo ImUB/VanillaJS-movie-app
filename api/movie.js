32607fe6ae9b636e2625b5903261bd66dc97a272
ImUB <dladbql1234@gmail.com>
2024-06-17 21:56:05 +0900

    서버리스 함수 추가

---
+++ api/movie.js
@@ -0,0 +1,13 @@
+import fetch from 'node-fetch'
+
+export default async function handler(request, response) {
+  const { title, page, id } = JSON.parse(request.body)
+  const url = id 
+    ? `https://omdbapi.com?apikey=91d05104&i=${id}&plot=full`
+    : `https://omdbapi.com?apikey=91d05104&s=${title}&page=${page}`
+  const res = await fetch(url)
+  const json = await res.json()
+  response
+  .status(200)
+  .json(json)
+}
