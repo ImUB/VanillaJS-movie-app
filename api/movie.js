33cd632c67c6153c6fda3eb33fdf05853dad5858
ImUB <dladbql1234@gmail.com>
2024-06-23 22:00:06 +0900

    .env 파일 추가

--- api/movie.js
+++ api/movie.js
@@ -1,10 +1,11 @@
 import fetch from 'node-fetch'
 
+const { APIKEY } = process.env
 export default async function handler(request, response) {
   const { title, page, id } = JSON.parse(request.body)
   const url = id 
-    ? `https://omdbapi.com?apikey=91d05104&i=${id}&plot=full`
-    : `https://omdbapi.com?apikey=91d05104&s=${title}&page=${page}`
+    ? `https://omdbapi.com?apikey=${APIKEY}&i=${id}&plot=full`
+    : `https://omdbapi.com?apikey=${APIKEY}&s=${title}&page=${page}`
   const res = await fetch(url)
   const json = await res.json()
   response
